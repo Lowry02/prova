1c2e6d76ac56c845d705a263719fe66f556a1ccc
Lowry02 <46626571+Lowry02@users.noreply.github.com>
2025-01-17 00:40:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,3 @@
-from ultralytics import YOLO
 from streamlit_webrtc import webrtc_streamer
 
-
-weights_path = './yolov11-seg2.pt'
-model = YOLO(weights_path)
-print("fatto")
 webrtc_streamer(key="sample")
