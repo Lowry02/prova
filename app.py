dcf55c6c0f022fc64a9aa75b7cb682ed5dc14475
Lowry02 <46626571+Lowry02@users.noreply.github.com>
2025-01-17 01:42:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
 from streamlit_webrtc import webrtc_streamer
 
-webrtc_streamer(key="sample")
+webrtc_streamer(
+    key="example",
+    rtc_configuration={
+        "iceServers": [
+            {"urls": ["stun:stun.l.google.com:19302"]},
+        ]
+    }
+)
